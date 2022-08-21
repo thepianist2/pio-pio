06802f1ba95ae4f8501c6d4f5379dc5576b3f9d6
Fabian Allel <fallel@MacBook-Pro.local>
2022-08-21 20:22:59 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
+
+
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Home from './Home/Home';
+import Login from './Login/Login';
+import MyProfile from './MyProfile/MyProfile';
+import Profile from './Profile/Profile';
+import Register from './Register/Register';
+
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
+    const [drawOpened, setDrawOpened] = useState(false);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          </Typography>
+          <MenuIcon onClick={()=>setDrawOpened(!drawOpened)} />
+        </Toolbar>
+        </AppBar>
+        <Drawer anchor={'right'} open={drawOpened} onClose={()=>setDrawOpened(!drawOpened)}>{<li>Hola</li>}</Drawer>
+        <Header></Header>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/myprofile" element={<MyProfile />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = ()=> {
+    return <header className="header-wrapper"><h1>Pio Pio</h1>
+    <img src="./images/piopio.png"/>
+    </header>
+}
+
+export default Header;
